949231d15ffbb1a8354d488ece329a033a87f99c
mirai <kkas0807@naver.com>
2021-11-28 15:50:36 +0900

    2021-11-28

---
+++ Jet/resources/js/Pages/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="main" id="app">
+
+        <div class="container">
+           <h1>efefe</h1>
+        </div>
+
+    </div>
+</template>
---
+++ Jet/resources/js/Pages/Profile/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mainbox">
+        <div class="homeimg">
+            <!-- <img src="/img/d2.png" class="dag"> -->
+        </div>
+
+        <h2>dasdsfs</h2>
+
+        <!-- <Homeimg/> -->
+    </div>
+</template>
+<script>
+
+
+export default {
+    components: {
+
+        // Homeimg
+
+    },
+};
+</script>
+<style>
+
+</style>
