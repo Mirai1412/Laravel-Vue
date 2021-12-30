73f14efeadadababcfb855b8f1cad3e70a28b558
mirai <kkas0807@naver.com>
2021-12-30 21:05:58 +0900

    add scss import

--- mypage/src/App.vue
+++ mypage/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app>
     <v-main>
-    <div class="mainbox">
+    <div id="mainbox">
 
-      <h1 class="titles">Portfolio to Mirai</h1>
-      <h1 class="subtitles">Yeungjin College</h1>
+      <h1 id="titles">Portfolio to Mirai</h1>
+      <h1 id="subtitles">Yeungjin College</h1>
 
-
-
-      <div class="flower"></div>
+      <div id="flower">
+        <font-awesome-icon icon="spinner" size="xs" />
+        <!-- <font-awesome-icon icon="user-secret" /> -->
+        <h1>asdad</h1>
+      </div>
 
     </div>
     </v-main>
@@ -16,85 +18,22 @@
 </template>
 
 <script>
-import Tree from  './components/Tree/tree.vue';
 export default {
   name: 'App',
 
   components: {
-  Tree
+
   },
 
 };
 </script>
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Vujahday+Script&display=swap');
+@import '@/assets/scss/flower.scss';
+@import '@/assets/scss/main.scss';
+
 body{
   margin: 0 auto;
   max-height: 719.2px;
-}
-.mainbox{
-  background-image: url("../public/img/bsdf.jpg");
-  background-size: cover;
-  background-position: center;
-  width: 100vw;
-  height: 100vh;
-  font-family: 'Vujahday Script', cursive;
-
-  .titles{
-    width: 100%;
-    position: absolute;
-    font-size: 7vmax;
-    text-align: center;
-    color: rgb(248, 110, 133);
-    // -webkit-text-stroke: 3px white;
-    animation-name: titles;
-    animation-duration: 2s;
-    animation-fill-mode: forwards;
-    
-  }
-  @keyframes titles {
-    	100%{top:4vmax;}
-    	0%{
-        top:20vmax;
-        opacity: 0;
-      }
-  }
-
-  .subtitles{
-    width: 100%;
-    position: absolute;
-    font-size: 4vmax;
-    text-align: center;
-    color: rgb(243, 120, 141);
-    // -webkit-text-stroke: 3px white;
-    opacity: 0;
-    animation-name: subtitles;
-    animation-duration: 2s;
-    animation-delay: 1s;
-    animation-fill-mode: forwards;
-  }
-    @keyframes subtitles {
-    	100%{
-        top:13vmax;
-        opacity: 1;
-        }
-    	0%{
-        top:30vmax;
-        opacity: 0;
-      }
-  }
-
-  .flower{
-    position: absolute;
-    top: 300px;
-    left: 300px;
-    width: 50px;
-    height: 50px;
-    
-    background-color: black;
-  }
-
-
 }
 
 </style>
--- mypage/src/main.js
+++ mypage/src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import '@/assets/scss/flower.scss';
+//fort-awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
+
+Vue.config.productionTip = false
+
+library.add(faUserSecret, faFontAwesome)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
